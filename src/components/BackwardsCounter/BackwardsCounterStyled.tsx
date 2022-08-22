230c877894dfb4beb1ba1e7ab695b46ffbb5e63f
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-22 16:22:04 +0200

    Added style to components

---
+++ src/components/BackwardsCounter/BackwardsCounterStyled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import "@fontsource/oxanium";
+
+const BackwardsCounterStyled = styled.ul`
+  background-color: #243342;
+  width: 500px;
+  height: 300px;
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  justify-content: center;
+  align-items: center;
+  font-family: Oxanium;
+`;
+
+export default BackwardsCounterStyled;
---
+++ src/components/CounterSection/CounterSectionStyled.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const CounterSectionStyled = styled.li`
+  height: 50%;
+  flex: 1;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  color: #dce0e3;
+  .counter__number {
+    background-color: #ffc700;
+    height: 80%;
+    width: 90%;
+    margin: 0 5%;
+    border-radius: 25px;
+    align-items: center;
+    justify-content: center;
+    justify-self: center;
+    align-self: center;
+    display: flex;
+    color: rgb(48, 48, 118);
+    font-size: 2rem;
+  }
+`;
+
+export default CounterSectionStyled;
