d93d4f31e6c5aecc956f2bf79d684d062e24b508
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-25 12:31:12 +0200

    Added condition to conditionally render either counter or heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,6 @@ import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const App = (): JSX.Element => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/:home" element={<Layout />} />
-        <Route path="/:register" element={<Layout />} />
-      </Routes>
-      <Layout />
-    </BrowserRouter>
-  );
+  return <Layout />;
 };
 export default App;
